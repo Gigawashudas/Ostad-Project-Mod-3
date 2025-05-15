cab143e14a109e2d014f9967fd74ef5dd502bf09
Gigawashu Das <hreevud@gmail.com>
2025-05-15 16:41:27 +0600

    without lines

--- ostad-project-mod-3.py
+++ ostad-project-mod-3.py
@@ -1,6 +1,25 @@
-import pathlib
 
-demoFile = open("demoFile.txt", "r")
 
-print(demoFile.read())
+
+import os
+import qrcode
+
+def read_from_file(fileName):
+    with open(fileName, "r") as file:
+        data = file.read()
+    return data
+
+def generate_qrcode(data):
+    qr = qrcode.make(data)
+    qr.save("image.png")
+
+absFilePath = os.path.abspath("File.txt")
+fileName = os.path.basename(absFilePath)
+
+print(absFilePath)
+print(fileName)
+
+data = read_from_file(fileName)
+
+qr_code = generate_qrcode(data)
 
