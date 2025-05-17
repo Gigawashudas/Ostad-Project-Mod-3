0f4ee5da110dbbbe6bb6f65c89819d6587d99f42
Gigawashu Das <hreevud@gmail.com>
2025-05-17 14:45:50 +0600

    Smaller code sectionas

--- ostad-project-mod-3.py
+++ ostad-project-mod-3.py
@@ -1,25 +1,16 @@
-
-
-
 import os
 import qrcode
 
-def read_from_file(inputFileName):
-    with open(inputFileName, "r") as file:
-        lines = file.readlines()
-        url = lines[0].strip()
-        fileName = lines[1].strip()
-        return url, fileName
+def read_from_file(inputFileName):  
+    with open(inputFileName, "r") as file:  
+        lines = map(str.strip, file.readlines())
+        return lines
 
 def generate_qrcode(url, fileName):
-    fileName = "-".join(fileName.split(" "))
     qr = qrcode.make(url)
-    qr.save(fileName + ".png")
+    qr.save("_".join(fileName.split(" ")) + ".png")
     print(f"QR code generated and saved as {fileName}")
 
-[url , fileName] = read_from_file(os.path.abspath("user_data.txt"))
-
-print(url + "\n" + fileName)
+[url, fileName] = read_from_file(os.path.abspath("user_data.txt"))
 
 generate_qrcode(url, fileName)
-
